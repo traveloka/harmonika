fba2258fa9a5a8a8c23ff41263a3466b889e4471
Sem <satryo@traveloka.com>
2015-08-03 20:07:29 +0700

    add JSON and console as identified Object

--- src/transformation/implicit-importer.js
+++ src/transformation/implicit-importer.js
@@ -24,7 +24,7 @@ export default
   }
 
 var unidentifiedIdentifier = [],
-  identifiedIdentifier = ['Object', 'console'],
+  identifiedIdentifier = ['Object', 'console', 'JSON'],
   defaultImportSource = {
     '$' : 'jQuery'
   };
