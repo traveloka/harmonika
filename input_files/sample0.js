3530f336f7257646e726b8bd37d8df474ae6a48f
semmatabei <sem.matabei@gmail.com>
2015-08-14 20:08:01 +0700

    Support flow-type checker notation

--- input_files/sample0.js
+++ input_files/sample0.js
@@ -1,6 +1,6 @@
 /**
  * Created by semmatabei on 8/13/15.
  */
-function plus(obj1: number, obj2: number): number {
+function plus(obj: number, obj0: Number, obj1: Array<number>, obj2: Class<MyClass, T>): number {
   return 0;
 }
